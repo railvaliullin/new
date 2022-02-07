9d93b7c27393e3773119b24979cf9bd32a66b4fc
svasenkov <aanher@gmail.com>
2022-02-07 22:16:31 +0300

    added selenide examples

--- src/test/java/tests/TextBoxTest.java
+++ src/test/java/tests/TextBoxTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTest {
 
