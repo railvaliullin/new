83c6003a5c57fd41b10e1d0b979a3412a0e53420
railvaliullin <railvaliullin@gmail.com>
2022-02-11 13:32:10 +0300

    demoqa-tests

--- src/test/java/tests/StudentRegistationFormTests.java
+++ src/test/java/tests/StudentRegistationFormTests.java
@@ -4,11 +4,14 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class StudentRegistationFormTests {
+
+
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
@@ -24,18 +27,27 @@ public class StudentRegistationFormTests {
         $(byText("Male")).click();
         $("#userNumber").setValue("0123456789");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("0");
-        $(".react-datepicker__year-select").selectOptionByValue("2000");
-        $(".react-datepicker__week ").$(byText("1")).click();
+        $(".react-datepicker__month-select").selectOptionByValue("1");
+        $(".react-datepicker__year-select").selectOptionByValue("1990");
+        $(".react-datepicker__day--029").click();
         $("#subjectsInput").setValue("Maths").pressEnter();
         $(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("852.jpg");
-        $("#currentAddress").setValue("Ulica, dom 28");
+        $("#currentAddress").setValue("Ulica");
         $("#state").click();
         $(byText("NCR")).click();
         $("#city").click();
         $(byText("Delhi")).click();
         $("#submit").click();
+        $(".table").shouldHave(text("Rail"));
+        $(".table").shouldHave(text("Valiullin"));
+        $(".table").shouldHave(text("testrail@mail.ru"));
+        $(".table").shouldHave(text("Male"));
+        $(".table").shouldHave(text("0123456789"));
+        $(".table").shouldHave(text("29 January,1990"));
+        $(".table").shouldHave(text("852.jpg"));
+        $(".table").shouldHave(text("NCR"));
+        $(".table").shouldHave(text("Delhi"));
 
 
     }
